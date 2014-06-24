8503e7192cc6eb19654274deef5e7178d7d9b82d
Josh Manders <josh@joshmanders.com>
2014-06-23 19:41:38 -0500

    additional changes to whoops

--- src/Providers/WhoopsServiceProvider.php
+++ src/Providers/WhoopsServiceProvider.php
@@ -9,7 +9,7 @@ class WhoopsServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
     {
-        $app['whoops.error_page_handler'] = function()
+        $app['whoops.pretty_page'] = function()
         {
             return new PrettyPageHandler;
         };
@@ -20,6 +20,7 @@ class WhoopsServiceProvider implements ServiceProviderInterface
             $run->pushHandler($app['whoops.error_page_handler']);
             return $run;
         };
+        $app['whoops.pretty_page']->setPageTitle('Something broke!');
         $app['whoops']->register();
     }
 }
